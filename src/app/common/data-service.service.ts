de3c5572cd4fb32edde32515d3c7f4e3b7bf8fff
unknown <49579991@qq.com>
2018-01-25 21:25:45 +0800

    'secondcommit'

--- src/app/common/data-service.service.ts
+++ src/app/common/data-service.service.ts
@@ -2,31 +2,29 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DataService {
-	//热搜
-	public resou:Array<any> = ['休闲娱乐','美食餐饮','便民服务'];
 	//导航菜单
 	public navbar:Array<any> = ['首页','市场活动','业务范围','关于我们'];
 	//底部字段
 	public footerList:Array<any>=[
 		{
-			title:'服务热线',
-			value:'13713967665',
-			icon:'phone1'
-		},
-		{
-			title:'公司邮箱',
-			value:'820352325@qq.com',
-			icon:'email1'
-		},
-		{
-			title:'公司传真',
-			value:'0755-26662863',
+			title:'联系人',
+			value:'方觧',
 			icon:'chuanzhen'
+		},
+		{
+			title:'联系电话',
+			value:'13826529652',
+			icon:'phone1'
 		},
 		{
 			title:'邮政编码',
 			value:'518100',
 			icon:'youbian'
+		},
+		{
+			title:'公司邮箱',
+			value:'331586401@qq.com',
+			icon:'email1'
 		},
 		{
 			title:'公司地址',
