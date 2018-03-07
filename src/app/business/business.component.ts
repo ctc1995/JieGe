563e4082eac2e785af5ab3031ca9c12c8b36ba18
unknown <49579991@qq.com>
2018-03-07 19:07:22 +0800

    ...

--- src/app/business/business.component.ts
+++ src/app/business/business.component.ts
@@ -16,17 +16,17 @@ export class BusinessComponent{
         },
         {
             "title":"“我是沙塘布人”系列活动表彰晚会",
-            "img":this.imgRootPath+"沙塘布2.JPG",
+            "img":this.imgRootPath+"sha2.JPG",
             "video":"http://mudu.tv/show/videolink2/322190/origin"
         },
         {
             "title":"深圳市清远商会联合年会暨清远市招商推介会",
-            "img":this.imgRootPath+"清远商会.jpg",
+            "img":this.imgRootPath+"qingyuan.jpg",
             "video":"http://mudu.tv/show/videolink2/315200/origin"
         },
         {
             "title":"宝岭社区文明家庭表彰晚会",
-            "img":this.imgRootPath+"宝岭晚会 .JPG",
+            "img":this.imgRootPath+"baoling .JPG",
             "video":"http://mudu.tv/show/videolink2/254778/origin"
         }
     ]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,44 +12,44 @@ export class HomeComponent implements OnInit {
 	business:Array<object>=[
 		{
 			"str":"吃货Follow Me",
-			"img":this.imgRootPath+"吃货.jpg",
+			"img":this.imgRootPath+"chihuo1.jpg",
 			"video":"http://mudu.tv/show/videolink2/267503/origin"
 		},
 		{
 			"str":"深圳市清远商会联合年会",
-			"img":this.imgRootPath+"清远商会.jpg",
+			"img":this.imgRootPath+"qingyuan.jpg",
 			"video":"http://mudu.tv/show/videolink2/315200/origin"
 		},
 		{
 			"str":"卓依婷美廷家具见面会",
-			"img":this.imgRootPath+"卓依廷见面会.JPG",
+			"img":this.imgRootPath+"zhuo.JPG",
 			"video":"http://mudu.tv/show/videolink2/255123/origin"
 		},
 		{
 			"str":"美廷家具明星团购惠",
-			"img":this.imgRootPath+"美廷明星团购会.JPG",
+			"img":this.imgRootPath+"meiting.JPG",
 			"video":"http://mudu.tv/show/videolink2/303200/origin"
 		}
 	];
 	land:Array<object>=[
 		{
 			"str":"西部雏鹰公益12周年",
-			"img":this.imgRootPath+"西部雏鹰.JPG",
+			"img":this.imgRootPath+"xibu.JPG",
 			"video":"http://mudu.tv/show/videolink2/307085/origin"
 		},
 		{
 			"str":"“我是沙塘布人”系列活动表彰晚会",
-			"img":this.imgRootPath+"沙塘布1.JPG",
+			"img":this.imgRootPath+"sha1.JPG",
 			"video":"http://mudu.tv/show/videolink2/322190/origin"
 		},
 		{
 			"str":"宝岭社区文明家庭表彰晚会",
-			"img":this.imgRootPath+"宝岭晚会 .JPG",
+			"img":this.imgRootPath+"baoling .JPG",
 			"video":"http://mudu.tv/show/videolink2/254782/origin"
 		},
 		{
 			"str":"龙岗街道“扫黄打非”主题征文颁奖暨进校园宣传活动",
-			"img":this.imgRootPath+"打黄扫非 .jpg",
+			"img":this.imgRootPath+"saohuang.jpg",
 			"video":"http://mudu.tv/show/videolink2/298591/origin"
 		}
 	];
